d7ddfb19c38e8b77502ae2bd0ff76e91a992680d
Muaz Aqdas <muazaqdasssh@gmail.com>
2022-07-08 02:39:56 +0530

    added icons and rotate function to aboutMe

--- src/Components/homeComponents/About.js
+++ src/Components/homeComponents/About.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, {useState} from "react";
 
 
 function AboutMe(props){
+
+  const [ rotate , letsRotate] = useState(360);
     return (
         <div id="aboutme" className="jumbotron jumbotron-fluid m-0" style={{ backgroundColor: "#D3EBCD" }}>
           <div className="container container-fluid">
@@ -12,6 +14,12 @@ function AboutMe(props){
               <div className= "col-lg-7">
                 <h2 className="display-4 mb-5 text-center">{props.heading}</h2>
                 <p className="lead text-center">{props.message}</p>
+                <button className="btn btn-outline-info" onClick={ ()=>{
+                    letsRotate(rotate + 360)
+                    document.getElementById("aboutme").style.WebkitTransitionDuration='1s';
+                    // document.getElementById("aboutme").style.transform = '';
+                    document.getElementById("aboutme").style.transform = `rotate(${rotate}deg)`;
+                }}>Lets Rotate</button>
               </div>
             </div>
           </div>
--- src/Components/homeComponents/GetInTouch.js
+++ src/Components/homeComponents/GetInTouch.js
@@ -1,4 +1,8 @@
 import React from "react";
+import {GiBugleCall} from "react-icons/gi"
+import {RiPencilRulerFill} from "react-icons/ri"
+import {SiMinutemailer} from "react-icons/si"
+
 
 const GetInTouch = (props) => {
   return (
@@ -6,6 +10,13 @@ const GetInTouch = (props) => {
       <h2 className="display-4 pb-3 text-center">{props.heading}</h2>
       <p className="lead text-center pb-3">
         {props.message}, <a href={`mailto:${props.email}`}>{props.email}</a>.
+      <div>
+      <GiBugleCall className="mr-4 mt-4" size={"40px"}/>
+      <RiPencilRulerFill className="mr-4 mt-4" size={"35px"}/>
+      <SiMinutemailer className="mr-4 mt-4" size={"35px"}/>
+
+      </div>
+        
       </p>
     </>
   );
